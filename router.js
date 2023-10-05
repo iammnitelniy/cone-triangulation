5b4b2936609e1a89dde3a3d3ebbdc81c3cab800f
ILIABARABANOV <ilianardan@gmail.com>
2023-10-05 16:12:59 +0300

    - router refactor, post req

--- router.js
+++ router.js
@@ -2,7 +2,10 @@ import Router from 'express'
 import ConeController from "./server/ConeController.js";
 const router = new Router()
 
-router.post('/coneParams', ConeController.create)
+
+
+router.post('/coneParams',ConeController.create)
+
 
 
 export default router;
